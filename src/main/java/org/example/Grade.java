29ce94ca1ce87facf61c89f0790e7ec0229b52a5
Volodymyr Nakonechnyi <volodymyr.nakonechnyi28@gmail.com>
2023-11-02 16:51:19 +0200

    UniversityTask

---
+++ src/main/java/org/example/Grade.java
@@ -0,0 +1,10 @@
+package org.example;
+import java.util.List;
+import java.util.Date;
+public class Grade {
+    private Subject subject;
+    private Date examDate;
+    private int grade;
+    private boolean isExam; // Exam - true; offset - false;
+    private boolean isPassed;
+}
---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,10 @@
+package org.example;
+import java.util.List;
+public class Student {
+    private int studentId;
+
+    private String lastName;
+    private String firstName;
+    private String group;
+    private List<Grade> grades;
+}
