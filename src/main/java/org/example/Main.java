cabec00ebd5ec360f185b06e7bce27fcd080458a
Volodymyr Nakonechnyi <volodymyr.nakonechnyi28@gmail.com>
2023-11-07 16:50:31 +0200

    Task-2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,57 +1,79 @@
 package org.example;
 
-import java.util.HashSet;
-import java.util.Set;
-
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 /*
-* Write parameterized methods union(Set set1, Set set2) and intersect(Set set1, Set set2),
-*  realizing the operations of union and intersection of two sets.
-*  Test the operation of these techniques on two pre-filled sets.
+* Create map personMap and add to it ten persons of type <String, String> (lastName, firstName).
+* Output the entities of the map on the screen.
+* There are at less two persons with the same firstName among these 10 people?
+* Remove from the map person whose firstName is "Orest" (or other). Print result.
 * */
-class Union {
-    public static <T> Set<T> union(Set<T>... args) {
-        Set<T> unionSet = new HashSet<T>();
 
-        for (Set<T> set : args) {
-            unionSet.addAll(set);
-        }
-
-        return unionSet;
-    }
-
-    public static<T> Set<T> intersect(Set <T> ... args) {
-        Set<T> intersectSet = new HashSet<T>(args[0]);
-
-        for (Set<T> sets: args){
-            intersectSet.retainAll(sets);
-        }
-
-        return intersectSet;
-    }
-}
 
 public class Main {
     public static void main(String[] args) {
-        Set<Integer> set1 = new HashSet<>();
-        set1.add(1);
-        set1.add(2);
-        set1.add(3);
+        Map<String, String> personMap = new HashMap<>();
 
-        Set<Integer> set2 = new HashSet<>();
-        set2.add(3);
-        set2.add(4);
-        set2.add(5);
+        personMap.put("Kalevski", "Orest");
+        personMap.put("Miloshevich", "Gena");
+        personMap.put("Lagarp", "Michele");
+        personMap.put("Bernardot", "Zan");
+        personMap.put("Nakonechyi", "Orest");
+        personMap.put("Fifa", "Micha");
+        personMap.put("Ubinav", "Vlad");
+        personMap.put("Micron", "Vlad");
+        personMap.put("Irlandian", "Gena");
+        personMap.put("Dombrovski", "Zan");
 
-        Set<Integer> set3 = new HashSet<>();
-        set3.add(2);
-        set3.add(3);
-        set3.add(6);
+        displayMap(personMap);
 
-        Set<Integer> unionResult = Union.union(set1, set2, set3);
-        System.out.println("Union of set1, set2, and set3: " + unionResult);
+        System.out.println("\nThere are at less two persons with the same firstName among these 10 people?");
 
-        Set<Integer> intersectResult = Union.intersect(set1, set2, set3);
-        System.out.println("Intersection of set1, set2, and set3: " + intersectResult);
+        if(hasDuplicateFirstName(personMap)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
 
+        System.out.println('\n');
+
+        deleteByFirstName(personMap);
+
+        displayMap(personMap);
+    }
+
+    static void deleteByFirstName(Map<String, String> personMap) {
+        Iterator<Map.Entry<String, String>> iterator = personMap.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<String, String> entry = iterator.next();
+            if (entry.getValue().equals("Orest")) {
+                iterator.remove();
+            }
+        }
+    }
+
+    static <T, U> void displayMap(Map<T, U> map) {
+        for (Map.Entry<T, U> entity : map.entrySet()) {
+            System.out.println(entity.getKey() + " " + entity.getValue());
+        }
+    }
+
+    static boolean hasDuplicateFirstName(Map<String, String> personMap) {
+        Map<String, Integer> firstNameCountMap = new HashMap<>();
+
+        for (String firstName : personMap.values()) {
+            int count = firstNameCountMap.getOrDefault(firstName, 0);
+
+            count++;
+            firstNameCountMap.put(firstName, count);
+            
+            if (count >= 2) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
+
