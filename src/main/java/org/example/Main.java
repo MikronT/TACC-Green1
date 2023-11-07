3e436826c31abd93cf0520f8dfb8c2a6c4f2146a
Volodymyr Nakonechnyi <volodymyr.nakonechnyi28@gmail.com>
2023-11-07 16:59:09 +0200

    Task - 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,79 +1,66 @@
 package org.example;
 
-import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
-import java.util.Map;
-/*
-* Create map personMap and add to it ten persons of type <String, String> (lastName, firstName).
-* Output the entities of the map on the screen.
-* There are at less two persons with the same firstName among these 10 people?
-* Remove from the map person whose firstName is "Orest" (or other). Print result.
-* */
 
+class Student implements Comparable<Student> {
+    private String name;
+    private int course;
 
-public class Main {
-    public static void main(String[] args) {
-        Map<String, String> personMap = new HashMap<>();
-
-        personMap.put("Kalevski", "Orest");
-        personMap.put("Miloshevich", "Gena");
-        personMap.put("Lagarp", "Michele");
-        personMap.put("Bernardot", "Zan");
-        personMap.put("Nakonechyi", "Orest");
-        personMap.put("Fifa", "Micha");
-        personMap.put("Ubinav", "Vlad");
-        personMap.put("Micron", "Vlad");
-        personMap.put("Irlandian", "Gena");
-        personMap.put("Dombrovski", "Zan");
-
-        displayMap(personMap);
-
-        System.out.println("\nThere are at less two persons with the same firstName among these 10 people?");
-
-        if(hasDuplicateFirstName(personMap)) {
-            System.out.println("Yes");
-        } else {
-            System.out.println("No");
-        }
-
-        System.out.println('\n');
-
-        deleteByFirstName(personMap);
-
-        displayMap(personMap);
+    public Student(String name, int course) {
+        this.name = name;
+        this.course = course;
     }
 
-    static void deleteByFirstName(Map<String, String> personMap) {
-        Iterator<Map.Entry<String, String>> iterator = personMap.entrySet().iterator();
+    public String getName() {
+        return name;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    public static void printStudents(List<Student> students, int targetCourse) {
+        Iterator<Student> iterator = students.iterator();
         while (iterator.hasNext()) {
-            Map.Entry<String, String> entry = iterator.next();
-            if (entry.getValue().equals("Orest")) {
-                iterator.remove();
+            Student student = iterator.next();
+            if (student.getCourse() == targetCourse) {
+                System.out.println(student.getName());
             }
         }
     }
 
-    static <T, U> void displayMap(Map<T, U> map) {
-        for (Map.Entry<T, U> entity : map.entrySet()) {
-            System.out.println(entity.getKey() + " " + entity.getValue());
-        }
-    }
-
-    static boolean hasDuplicateFirstName(Map<String, String> personMap) {
-        Map<String, Integer> firstNameCountMap = new HashMap<>();
-
-        for (String firstName : personMap.values()) {
-            int count = firstNameCountMap.getOrDefault(firstName, 0);
-
-            count++;
-            firstNameCountMap.put(firstName, count);
-            
-            if (count >= 2) {
-                return true;
-            }
-        }
-
-        return false;
+    @Override
+    public int compareTo(Student otherStudent) {
+        return this.name.compareTo(otherStudent.name);
     }
 }
 
+public class Main {
+    public static void main(String[] args) {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Vova", 1));
+        students.add(new Student("Bogdan", 2));
+        students.add(new Student("Maria", 1));
+        students.add(new Student("Dasha", 3));
+        students.add(new Student("Anna", 2));
+
+        System.out.println("Students ordered by name:");
+        Collections.sort(students);
+        for (Student student : students) {
+            System.out.println(student.getName() + " - Course: " + student.getCourse());
+        }
+
+        System.out.println("\nStudents ordered by course:");
+        students.sort((s1, s2) -> Integer.compare(s1.getCourse(), s2.getCourse()));
+        for (Student student : students) {
+            System.out.println(student.getName() + " - Course: " + student.getCourse());
+        }
+
+        int targetCourse = 2;
+        System.out.println("\nStudents in course " + targetCourse + ":");
+        Student.printStudents(students, targetCourse);
+    }
+}
