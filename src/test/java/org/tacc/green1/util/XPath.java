40630ea87b906a4613ef938f4cde7b1df5bbb695
MikronT <vlad.ivanov.if@gmail.com>
2023-11-23 18:24:21 +0200

    Turn XPath into an interface

--- src/test/java/org/tacc/green1/util/XPath.java
+++ src/test/java/org/tacc/green1/util/XPath.java
@@ -1,13 +1,13 @@
 package org.tacc.green1.util;
 
-public class XPath {
-    public interface AccountSelect {
+public interface XPath {
+    interface AccountSelect {
         String MY_ACCOUNT_OPTION = "/html/body/div[2]/header/div[1]/div/ul/li[2]/div/ul/li[1]/a",
                 MY_WISH_LIST_OPTION = "/html/body/div[2]/header/div[1]/div/ul/li[2]/div/ul/li[2]/a",
                 SIGN_OUT_OPTION = "/html/body/div[2]/header/div[1]/div/ul/li[2]/div/ul/li[3]/a";
     }
 
-    public interface MainPage {
+    interface MainPage {
         String WELCOME_MESSAGE_MAIN_PAGE = "/html/body/div[2]/header/div[1]/div/ul/li[1]/span",
                 LINK_REGISTRATION = "/html/body/div[2]/header/div[1]/div/ul/li[3]/a";
     }
