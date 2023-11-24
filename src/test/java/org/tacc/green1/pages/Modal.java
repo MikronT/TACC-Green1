9c488f39114fba033739a7479e2e9a229bbccfe6
Volodymyr Stefunko <stefunkovolodymyr@gmail.com>
2023-11-24 16:42:14 +0200

    added chained timeout and redirect

--- src/test/java/org/tacc/green1/pages/Modal.java
+++ src/test/java/org/tacc/green1/pages/Modal.java
@@ -1,6 +1,8 @@
 package org.tacc.green1.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.tacc.green1.util.Utils;
 
 
 public abstract class Modal {
@@ -11,5 +13,14 @@ public abstract class Modal {
 
     public void quit() {
         modalDriver.quit();
+    }
+
+    public <T extends Modal> T redirect(Class<T> pageClass) {
+        return PageFactory.initElements(modalDriver, pageClass);
+    }
+
+    public <T extends Modal> T timeout(int seconds) {
+        Utils.sleep(seconds);
+        return (T) this;
     }
 }
