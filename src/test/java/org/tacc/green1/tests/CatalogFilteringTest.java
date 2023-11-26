11679b1f468039cf4c00d44c1a2cb3cecd2dac61
MikronT <vlad.ivanov.if@gmail.com>
2023-11-26 23:29:25 +0200

    Final fixes

--- src/test/java/org/tacc/green1/tests/CatalogFilteringTest.java
+++ src/test/java/org/tacc/green1/tests/CatalogFilteringTest.java
@@ -1,7 +1,5 @@
 package org.tacc.green1.tests;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -13,7 +11,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class CatalogFilteringTest {
-    private static final Logger LOG = LogManager.getLogger(CatalogFilteringTest.class);
     private static CatalogPage catalogPage;
 
 
