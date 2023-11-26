b388d9c31077e945e9b5de461212837d99f73fc1
MikronT <vlad.ivanov.if@gmail.com>
2023-11-26 22:17:00 +0200

    Add default 3s wait to Modal

--- src/test/java/org/tacc/green1/pages/Modal.java
+++ src/test/java/org/tacc/green1/pages/Modal.java
@@ -2,13 +2,23 @@ package org.tacc.green1.pages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.tacc.green1.util.Utils;
+
+import java.time.Duration;
 
 
 public abstract class Modal<T> {
     private static Actions modalActions = null;
+    private static WebDriverWait modalWait;
     protected static WebDriver modalDriver;
 
+
+    protected static WebDriverWait modalWait() {
+        if (modalWait == null)
+            modalWait = new WebDriverWait(modalDriver, Duration.ofSeconds(3));
+        return modalWait;
+    }
 
     protected static Actions modalActions() {
         if (modalActions == null)
