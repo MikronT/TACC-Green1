d9709ebe62932ff0bcd79bfc9f49cf6072ea3da9
MikronT <vlad.ivanov.if@gmail.com>
2023-11-27 13:33:56 +0200

    Revert "Use partial text locator for stability"
    This reverts commit da0066ac85ee32137f08a6b4a21ea035d1b058ee.

--- src/test/java/org/tacc/green1/model/authorized/components/AccountSidebar.java
+++ src/test/java/org/tacc/green1/model/authorized/components/AccountSidebar.java
@@ -6,13 +6,14 @@ import org.openqa.selenium.support.PageFactory;
 import org.tacc.green1.model.AccountInformationPage;
 import org.tacc.green1.model.AddressBookPage;
 import org.tacc.green1.model.Modal;
+import org.tacc.green1.util.XPath;
 
 
-public class AccountSidebar extends Modal<AccountSidebar> {
-    @FindBy(partialLinkText = "Address Book")
+public class AccountSidebar extends Modal<AccountSidebar> implements XPath.AccountSidebar {
+    @FindBy(xpath = LINK_ADDRESS_BOOK)
     private WebElement addressBookLink;
 
-    @FindBy(partialLinkText = "Account Information")
+    @FindBy(xpath = LINK_ACCOUNT_INFORMATION)
     private WebElement accountInformationLink;
 
 
