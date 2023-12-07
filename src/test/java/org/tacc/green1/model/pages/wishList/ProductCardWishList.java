3f65da6f1ad4f15055e61f75a81f2abb6a392c2d
VolodymyrNakonechnyi <146861887+VolodymyrNakonechnyi@users.noreply.github.com>
2023-12-07 15:14:15 +0200

    Add to ProductCardWishList labelName and price getters

--- src/test/java/org/tacc/green1/model/pages/wishList/ProductCardWishList.java
+++ src/test/java/org/tacc/green1/model/pages/wishList/ProductCardWishList.java
@@ -1,7 +1,22 @@
 package org.tacc.green1.model.pages.wishList;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.tacc.green1.model.base.Component;
+import org.tacc.green1.util.XPath;
 
-public class ProductCardWishList extends Component {
+public class ProductCardWishList extends Component implements XPath.ProductCardWishList {
+    @FindBy(xpath = LABEL_NAME)
+    private WebElement labelName;
 
+    @FindBy(xpath = PRICE)
+    private WebElement price;
+
+    public String getLabelName() {
+        return labelName.getText();
+    }
+
+    public String getPrice() {
+        return price.getText();
+    }
 }
